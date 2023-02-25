d4b5e39ec477c5796650de78d6fa2c5905cde512
AnasurRehman <anasurrehman12@gmail.com>
2023-02-25 22:05:55 +0500

    Updates

---
+++ precache-manifest.476ce8bf599b1f6bc98ee699c8d4824b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc02ec3325e9a63c77174984d60aed25",
+    "url": "/AurehRestaurant/index.html"
+  },
+  {
+    "revision": "4a2d6fd5d68dde00f458",
+    "url": "/AurehRestaurant/static/css/main.2d16dcd0.chunk.css"
+  },
+  {
+    "revision": "7b270f2c8f6ffd23ffbe",
+    "url": "/AurehRestaurant/static/js/2.da41bc5a.chunk.js"
+  },
+  {
+    "revision": "4a2d6fd5d68dde00f458",
+    "url": "/AurehRestaurant/static/js/main.309b6b3b.chunk.js"
+  },
+  {
+    "revision": "55bd311e66efe15c9421",
+    "url": "/AurehRestaurant/static/js/runtime-main.d1338172.js"
+  },
+  {
+    "revision": "6c19c29d7d1f9e9c12b08218ba2a80fd",
+    "url": "/AurehRestaurant/static/media/1.6c19c29d.jpg"
+  },
+  {
+    "revision": "6dfd6a8cc00186ee9085735a0d823b7a",
+    "url": "/AurehRestaurant/static/media/2.6dfd6a8c.jpg"
+  },
+  {
+    "revision": "c8dc247a6bd877a826250cd434fcf664",
+    "url": "/AurehRestaurant/static/media/3.c8dc247a.jpg"
+  },
+  {
+    "revision": "4b50bccaa00ac6bc3c21ec2d312b6ac4",
+    "url": "/AurehRestaurant/static/media/cd.4b50bcca.jpg"
+  },
+  {
+    "revision": "37615d7a5e0943376557522160fc315e",
+    "url": "/AurehRestaurant/static/media/dinner.37615d7a.jpg"
+  },
+  {
+    "revision": "b66a9de4432ae443bd70be17dc410805",
+    "url": "/AurehRestaurant/static/media/dinner1.b66a9de4.jpg"
+  },
+  {
+    "revision": "084571d5a7916ad68089361665622562",
+    "url": "/AurehRestaurant/static/media/drink.084571d5.jpg"
+  },
+  {
+    "revision": "9861d981dba94278c78ca70b9f8b406f",
+    "url": "/AurehRestaurant/static/media/fastfood.9861d981.jpg"
+  },
+  {
+    "revision": "6a8e667731d188aba425d3e98efa5bea",
+    "url": "/AurehRestaurant/static/media/fb.6a8e6677.png"
+  },
+  {
+    "revision": "f6c0eda5fa88ef6f7ddfc7a1be731ace",
+    "url": "/AurehRestaurant/static/media/ff.f6c0eda5.jpg"
+  },
+  {
+    "revision": "d59c3272f82bbc6ebb821b5784004843",
+    "url": "/AurehRestaurant/static/media/gallery1.d59c3272.jpg"
+  },
+  {
+    "revision": "298e9669e28112a54115be0933a1fc1d",
+    "url": "/AurehRestaurant/static/media/ice2.298e9669.png"
+  },
+  {
+    "revision": "c35ec174ab69cbe7d69fda4804fac02a",
+    "url": "/AurehRestaurant/static/media/icecreame.c35ec174.jpg"
+  },
+  {
+    "revision": "778c3951b6f5dac1f13b6d13e00938a8",
+    "url": "/AurehRestaurant/static/media/image_6.778c3951.jpg"
+  },
+  {
+    "revision": "a570faaffbd41b83add4f0e06a27babd",
+    "url": "/AurehRestaurant/static/media/logo.a570faaf.png"
+  },
+  {
+    "revision": "caa2d4ce1efd8b3c5513df7ab699c9cf",
+    "url": "/AurehRestaurant/static/media/lunch2.caa2d4ce.jpg"
+  },
+  {
+    "revision": "36e1cb435d9e5c1ccaf5ec7de2d8ab17",
+    "url": "/AurehRestaurant/static/media/myteam.36e1cb43.jpg"
+  },
+  {
+    "revision": "cb4a1524b3c26737189a1dc4e69a5ec9",
+    "url": "/AurehRestaurant/static/media/party1.cb4a1524.svg"
+  },
+  {
+    "revision": "d9813d1d215caa623e0209ab3f804850",
+    "url": "/AurehRestaurant/static/media/restaurant1.d9813d1d.svg"
+  },
+  {
+    "revision": "6dc927288da69020f5360a25cc13e1bd",
+    "url": "/AurehRestaurant/static/media/restaurant4.6dc92728.svg"
+  }
+]);
